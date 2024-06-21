e2b2127eb0902c146af997bde8880e82337e4845
suranjitpaul84 <suranjit.paul84@gmail.com>
2024-06-20 19:55:03 -0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://9kq5f38gll.execute-api.us-east-1.amazonaws.com/default/demoLambda-1'
+const API_ENDPOINT = 'https://9qkqh458t9.execute-api.us-east-1.amazonaws.com/default/demoLambda-1'
 
 export default {
   name: 'app',
